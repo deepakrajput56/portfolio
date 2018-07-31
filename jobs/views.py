b0dbad5fac435a2c3d4603f7bfc68495bef41d03
Deepak Rajput <deepak.rajput56@gmail.com>
2018-07-31 13:10:16 +0530

    latest commit

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Jobs
+def home(request):
+    jobs= Jobs.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
 
-# Create your views here.
